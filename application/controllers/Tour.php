e019870694f53f4f971c79ccf1ebe23ea936803b
5540011055 <izudsuarez@gmail.com>
2018-01-06 11:04:43 +0700

    xxxxx

--- application/controllers/Tour.php
+++ application/controllers/Tour.php
@@ -9,22 +9,21 @@ class Tour extends CI_Controller {
 
 public function index()
 	{
-	//echo 123;
-		/*$params = json_decode(file_get_contents('php://input'), TRUE);
-		if(isset($params)){
-				$data['results'] = $this->Product_model->loaddata($params);
-				$this->load->view('product_view',$data);
-		}else{
-			
-			echo "Please check your json from";
-		}*/
+
 		$params = json_decode(file_get_contents('php://input'), TRUE);
 		$data = $this->Tour_model->queryData($params);
-//		echo json_encode($data);
+
 		json_output($data['status'],$data['response']);
 		
 	}
 
+public function get_detail()
+	{
+		
+		$params = json_decode(file_get_contents('php://input'), TRUE);
+		$data = $this->Tour_model->query_detail($params);
+		json_output($data['status'],$data['response']);
+	}
 
 }
 ?>
--- application/models/Tour_model.php
+++ application/models/Tour_model.php
@@ -5,45 +5,52 @@ class Tour_model extends CI_Model {
 
 
   	public function queryData($param) {
-		 $select = array('tbl_web_product.topic', 
-'tbl_web_product.detail_en', 
-'tbl_web_product.detail_cn',
-'tbl_web_product.detail_th', 
-'tbl_web_product.cost_a_agent_all',
-'tbl_web_product.cost_b_agent_all', 
-'tbl_web_product.type', 
-'tbl_web_product.topic_en_web as topic_en',
-'tbl_web_product.topic_cn_web as topic_cn',
-'tbl_web_product.topic_th_web as topic_th',
-'tbl_web_product.onsale_front', 
-'tbl_web_product.onsale_enable', 
-'tbl_web_product.onsale_promotion', 
-'tbl_web_product.onsale_top',
-'tbl_web_product.image_crop',
-'tbl_web_product.id',
-'tbl_web_product.round_en',
-'tbl_web_product.province',
-'tbl_web_product.open_Sun',
-'tbl_web_product.open_Mon', 
-'tbl_web_product.open_Tue',
-'tbl_web_product.open_Wed',
-'tbl_web_product.open_Thu',
-'tbl_web_product.open_Fri',
-'tbl_web_product.open_Sat',
-'tbl_web_admin.company',
-'tbl_web_product.province',
-'tbl_province.name_th as province_name');
+		 $select = array(
+		 'tbl_web_product.topic', 
+		//'tbl_web_product.detail_en', 
+		//'tbl_web_product.detail_cn',
+		//'tbl_web_product.detail_th', 
+		'tbl_web_product.cost_a_agent_all',
+		'tbl_web_product.cost_b_agent_all', 
+		'tbl_web_product.type', 
+		//'tbl_web_product.topic_en_web as topic_en',
+		//'tbl_web_product.topic_cn_web as topic_cn',
+		//'tbl_web_product.topic_th_web as topic_th',
+		'tbl_web_product_utf.topic_th as topic_th' ,
+		'tbl_web_product_utf.topic_cn as topic_cn' ,
+		'tbl_web_product_utf.topic_en as topic_en' ,
+		'tbl_web_product.onsale_front', 
+		'tbl_web_product.onsale_enable', 
+		'tbl_web_product.onsale_promotion', 
+		'tbl_web_product.onsale_top',
+		'tbl_web_product.image_crop',
+		'tbl_web_product.id',
+		'tbl_web_product.round_en',
+		'tbl_web_product.province',
+		'tbl_web_product.open_Sun',
+		'tbl_web_product.open_Mon', 
+		'tbl_web_product.open_Tue',
+		'tbl_web_product.open_Wed',
+		'tbl_web_product.open_Thu',
+		'tbl_web_product.open_Fri',
+		'tbl_web_product.open_Sat',
+		'tbl_web_admin.company',
+		'tbl_web_product.province',
+		'tbl_province.name_th as province_name');
 
-		  $pd_type = $param[type];
-		  foreach ($select as $key => $value){
+		  	$pd_type = $param[type];
+		  	foreach ($select as $key => $value){
 				$this->db->select($value);
 			}	
 			
 			$this->db->from('web_product tbl_web_product'); 
+			$this->db->join('web_product_utf tbl_web_product_utf', 'tbl_web_product.id=tbl_web_product_utf.id', 'left');
 			$this->db->join('web_admin tbl_web_admin', 'tbl_web_product.company=tbl_web_admin.id', 'left');
 			$this->db->join('web_province tbl_province', 'tbl_web_product.province=tbl_province.id', 'left');
 		 	$this->db->where('tbl_web_product.onsale_enable =','1');
+		 	if($pd_type!=""){
 		 	$this->db->where('tbl_web_product.type like ',''.$pd_type.'');
+			}
 		 	$query = $this->db->get(); 
 		 	if($query->num_rows() > 0) {
 		 	 foreach($query->result() as $row){	
@@ -52,7 +59,7 @@ class Tour_model extends CI_Model {
 				$status = '202';
 				$messge = "Load Data Success";
 			 }else{
-			 		$data = false;
+			 		$data_query[] = false;
 			 		$status = '404';
 			 		$messge = "No data row";
 			 }
@@ -61,7 +68,10 @@ class Tour_model extends CI_Model {
 		 	$this->db->from('web_product t1'); 
 		 	$this->db->join('web_province t2', 't1.province=t2.id', 'left');
 		 	$this->db->where('t1.onsale_enable =','1');
-		 	$this->db->where('t1.type like ',''.$pd_type.'');
+		 	if($pd_type!=""){
+				$this->db->where('t1.type like ',''.$pd_type.'');
+			}
+		 	
       		$this->db->group_by('t1.province');
       		$query2 = $this->db->get(); 
 
@@ -81,6 +91,45 @@ class Tour_model extends CI_Model {
 		
 	}
 
+	
+	public function query_detail($param){
+		
+		$select = array(
+		'id',
+		/*'topic_en',
+		'topic_cn',
+		'topic_th',*/
+		'detail_en',
+		'detail_cn',
+		'detail_th'
+		);
+		$id = $param[id];
+		foreach($select as $key => $value){
+			$this->db->select($value);
+		}	
+		$this->db->from('web_product_utf');
+		$this->db->where('id = ',''.$id.''); 
+		$query = $this->db->get();
+		 
+		if($query->num_rows() > 0) {
+			foreach($query->result() as $row){  	
+					$result[] = $row;
+				}
+			$status = '202';
+			$messge = "Load Data Success";	
+		}else{
+			$result[] = false;
+			$status = '404';	
+			$messge = "No data row";
+		}
+		$data['status'] = $status;
+      	$data['messge'] =  $messge;
+      	$data['size'] =  sizeof($query->result());	
+      	$data['data'] = $result;
+      	$data['params'] = $param;
+      	
+		return array('status' => 200,"response"=>$data);
+	}
 	
 } 
 
